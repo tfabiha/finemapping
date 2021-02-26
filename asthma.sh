5d28cede5345cd75d7f0e2183c3a04ac1b770978
Tabassum Fabiha <tfabiha@macaroni.local>
2021-02-26 16:11:02 -0500

    made changes to run locally with docker

--- asthma.sh
+++ asthma.sh
@@ -6,5 +6,4 @@ sos run ~/finemapping/2020_finemap_test_data/SuSiE_test.ipynb \
     --region_file ~/finemapping/2020_finemap_test_data/0asthma.clumped_region \
     --sumstats_path ~/finemapping/2020_finemap_test_data/ukb_imp_v3.Asthma_casesbyICD10codesANDselfreport_controlsbyselfreportandicd10_noautoimmuneincontrols.fastGWA.snp_stats.gz \
     --N 230411 \
-    --container_lmm /gpfs/gibbs/pi/dewan/data/UKBiobank/lmm.sif
     $JOB_OPT
--- intbmi.sh
+++ intbmi.sh
@@ -9,6 +9,5 @@ sos run ~/finemapping/2020_finemap_test_data/SuSiE_test.ipynb \
     --region_file ~/finemapping/2020_finemap_test_data/0intbmi.clumped_region \
     --sumstats_path ~/finemapping/2020_finemap_test_data/ukb_imp_v3.UKB_caucasians_BMIwaisthip_AsthmaAndT2D_INT-BMI_withagesex_041720.BoltLMM.snp_stats.gz \
     --N 230411 \
-    --container_lmm /gpfs/gibbs/pi/dewan/data/UKBiobank/lmm.sif
     $JOB_OPT
 
