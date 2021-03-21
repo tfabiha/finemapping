a13a95a4c16d710f2abfce4cc59b7da49725444b
Tabassum Fabiha <tfabiha@macaroni.local>
2021-03-20 20:00:38 -0400

    added reading rds functionality and change in md information

--- asthma.sh
+++ asthma.sh
@@ -3,7 +3,7 @@
 sos run ~/finemapping/2020_finemap_test_data/SuSiE_test.ipynb \
     --cwd ~/finemapping/2020_finemap_test_data/asthma_output1_single \
     --region_dir ~/finemapping/2020_finemap_test_data/asthma_regions \
-    --region_file ~/finemapping/2020_finemap_test_data/1asthma.clumped_region \
+    --region_file ~/finemapping/2020_finemap_test_data/0asthma.clumped_region \
     --sumstats_path ~/finemapping/2020_finemap_test_data/ukb_imp_v3.Asthma_casesbyICD10codesANDselfreport_controlsbyselfreportandicd10_noautoimmuneincontrols.fastGWA.snp_stats.gz \
     --N 230411 \
     $JOB_OPT
