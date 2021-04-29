84b6408a78a38079cedcdc098855c900cd830d77
tfabiha <tfabiha01@gmail.com>
2021-04-29 14:16:50 -0400

    jazf1

--- asthma.sh
+++ asthma.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sos run ~/finemapping/2020_finemap_test_data/SuSiE_test.ipynb \
-    --cwd ~/finemapping/2020_finemap_test_data/asthma_output1_single \
-    --region_dir ~/finemapping/2020_finemap_test_data/asthma_regions \
-    --region_file ~/finemapping/2020_finemap_test_data/0asthma.clumped_region \
-    --sumstats_path ~/finemapping/2020_finemap_test_data/ukb_imp_v3.Asthma_casesbyICD10codesANDselfreport_controlsbyselfreportandicd10_noautoimmuneincontrols.fastGWA.snp_stats.gz \
+sos run ~/finemapping/2020_finemap_test_data/SuSiE_test-copy.ipynb \
+    --cwd ~/finemapping/jazf1 \
+    --region_dir /gpfs/gibbs/pi/dewan/data/UKBiobank/results/region_extraction/asthma \
+    --region_file ~/finemapping/jaz_file.clumped_region \
+    --sumstats_path /gpfs/gibbs/pi/dewan/data/UKBiobank/results/FastGWA_results/results_imputed_data/asthma/Asthma_casesbyICD10codesANDselfreport_controlsbyselfreportandicd10_noautoimmuneincontrols_forbolt030720_ASTHMA.fastGWA.snp_stats.gz \
     --N 230411 \
     --pip_cutoff 0.1 \
     $JOB_OPT
